aec958fd7338988495bc0141f53a5b5f5f21e9dd
hologram94 <hologram94@users.noreply.github.com>
2019-12-26 02:12:14 +0800

    Matlab Simulation

--- Simulation/threedofsim.m
+++ Simulation/threedofsim.m
@@ -1,35 +1,35 @@
-function varargout = Robot_Manipulator(varargin)
-% ROBOT_MANIPULATOR MATLAB code for Robot_Manipulator.fig
-%      ROBOT_MANIPULATOR, by itself, creates a new ROBOT_MANIPULATOR or raises the existing
+function varargout = threedofsim(varargin)
+% THREEDOFSIM MATLAB code for threedofsim.fig
+%      THREEDOFSIM, by itself, creates a new THREEDOFSIM or raises the existing
 %      singleton*.
 %
-%      H = ROBOT_MANIPULATOR returns the handle to a new ROBOT_MANIPULATOR or the handle to
+%      H = THREEDOFSIM returns the handle to a new THREEDOFSIM or the handle to
 %      the existing singleton*.
 %
-%      ROBOT_MANIPULATOR('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in ROBOT_MANIPULATOR.M with the given input arguments.
+%      THREEDOFSIM('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in THREEDOFSIM.M with the given input arguments.
 %
-%      ROBOT_MANIPULATOR('Property','Value',...) creates a new ROBOT_MANIPULATOR or raises the
+%      THREEDOFSIM('Property','Value',...) creates a new THREEDOFSIM or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before Robot_Manipulator_OpeningFcn gets called.  An
+%      applied to the GUI before threedofsim_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to Robot_Manipulator_OpeningFcn via varargin.
+%      stop.  All inputs are passed to threedofsim_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help Robot_Manipulator
+% Edit the above text to modify the response to help threedofsim
 
-% Last Modified by GUIDE v2.5 15-May-2017 22:18:27
+% Last Modified by GUIDE v2.5 26-Dec-2019 02:10:10
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @Robot_Manipulator_OpeningFcn, ...
-                   'gui_OutputFcn',  @Robot_Manipulator_OutputFcn, ...
+                   'gui_OpeningFcn', @threedofsim_OpeningFcn, ...
+                   'gui_OutputFcn',  @threedofsim_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,26 +44,26 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before Robot_Manipulator is made visible.
-function Robot_Manipulator_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before threedofsim is made visible.
+function threedofsim_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to Robot_Manipulator (see VARARGIN)
+% varargin   command line arguments to threedofsim (see VARARGIN)
 
-% Choose default command line output for Robot_Manipulator
+% Choose default command line output for threedofsim
 handles.output = hObject;
 
 % Update handles structure
 guidata(hObject, handles);
 
-% UIWAIT makes Robot_Manipulator wait for user response (see UIRESUME)
+% UIWAIT makes threedofsim wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = Robot_Manipulator_OutputFcn(hObject, eventdata, handles) 
+function varargout = threedofsim_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
